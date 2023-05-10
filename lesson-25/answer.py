4e0483a7c90c48fde852a4c545515a74d6bff7b2
Jon O'Connell <jjoc0205@gmail.com>
2023-05-09 23:47:11 -0400

    better tsp

--- lesson-25/answer.py
+++ lesson-25/answer.py
@@ -1,38 +1,35 @@
-def find_closest(lines: list[list[int]], current: int, visited: set[int]):
-    closest = -1
-    closest_dist = 1000
-    for i in range(len(lines)):
-        if i in visited:
-            continue
-        if lines[current][i] < closest_dist:
-            closest = i
-            closest_dist = lines[current][i]
-    return closest
-def greedy(lines: list[list[int]]):
+def tsp(lines, start):
+    n = len(lines)
     path = []
-    visited = set()
-    visited.add(0)
-    for i in range(len(lines)):
-        path.append(find_closest(lines, i, visited))
-        visited.add()
-    return sum(path)
-def TSP(lines: list[list[int]]):
-    visited = set()
-    path = []
-    visited.add(0)
-    current = 0
-    while len(visited) < len(lines):
-        path.append(current)
-        visited.add(current)
-    return 0
-
+    visited = [False] * n
+    visited[start] = True
+    path.append(start)
+    current = start
+    dist = 0
+    
+    for i in range(n-1):
+        next = -1
+        shortest = float("inf")
+        for j in range(n):
+            if not visited[j] and lines[current][j] < shortest:
+                next = j
+                shortest = lines[current][j]
+        visited[next] = True
+        path.append(next)
+        dist += shortest
+        current = next
+    
+    dist += lines[path[-1]][start]
+    path.append(start)
+    return path, dist
 
 if __name__=="__main__":
     filename = input()
 
     with open("input.txt") as data_file:
         lines:list[str] = data_file.readlines()
-    lines = [line.strip() for line in lines]
-    lines = [line.split() for line in lines]
+    lines = [line.strip().split() for line in lines]
     lines = [[eval(num) for num in line] for line in lines]
-    print(greedy(lines))
+    path, dist = tsp(lines, 0)
+    print(*path[:-1], sep="\n")
+    print(dist)
